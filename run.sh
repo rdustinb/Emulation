90df28de5b572bc8a47fe3b79ca5c559fd01f252
Dustin Brothers <gentian.limited-0t@icloud.com>
2025-03-21 10:02:24 -0600

    Configuration and run shell script now support multiple versions of the Emulation systems.

--- run.sh
+++ run.sh
@@ -3,14 +3,12 @@
 # Get this Script Location
 THISFOLDER=`dirname $0`
 
-echo "Running from the folder ${THISFOLDER}"
+THISVERSION=$1
 
 # Copy over the system configuration
-rsync -av ${THISFOLDER}/Configs/system.config ~/.basilisk_ii_prefs
+rsync -av ${THISFOLDER}/Configs/system${THISVERSION}.config ~/.basilisk_ii_prefs
 
 # Run the System
 /Applications/BasiliskII.app/Contents/MacOS/BasiliskII \
   --rom ${THISFOLDER}/ROMs/ColorClassic-1MB.rom \
-  --disk ${THISFOLDER}/Floppys/System7_Boot.dsk \
-  --disk ${THISFOLDER}/Floppys/System7.5.3_Install.dsk \
-  --disk ${THISFOLDER}/HDDs/HDD.dsk
+  --disk ${THISFOLDER}/HDDs/HDD${THISVERSION}.dsk
