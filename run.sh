bf642777847c0e7d6cb59c09049ada9c4f0bcb43
Dustin Brothers <gentian.limited-0t@icloud.com>
2025-03-21 17:45:31 -0600

    The UDP Port is now set dynamically when the basiliskii application is run from the terminal. This allows my local aliases to specify the port rather than the commited config file.

--- run.sh
+++ run.sh
@@ -4,11 +4,13 @@
 THISFOLDER=`dirname $0`
 
 THISVERSION=$1
+THISUDPPORT=$2
 
 # Copy over the system configuration
 rsync -av ${THISFOLDER}/Configs/system${THISVERSION}.config ~/.basilisk_ii_prefs
 
 # Run the System
 /Applications/BasiliskII.app/Contents/MacOS/BasiliskII \
+  --udpport ${THISUDPPORT} \
   --rom ${THISFOLDER}/ROMs/ColorClassic-1MB.rom \
   --disk ${THISFOLDER}/HDDs/HDD${THISVERSION}.dsk
